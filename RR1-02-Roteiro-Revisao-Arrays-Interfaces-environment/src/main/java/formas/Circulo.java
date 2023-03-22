5291dab31d47521995833cf9f98624677ec61505
crissalves <cristianalves_cg@hotmail.com>
2023-03-22 09:18:31 -0300

    Roteiro de leda.

---
+++ RR1-02-Roteiro-Revisao-Arrays-Interfaces-environment/src/main/java/formas/Circulo.java
@@ -0,0 +1,16 @@
+package formas;
+
+public class Circulo implements Forma{
+
+    private double raio;
+
+    public Circulo(double raio){
+        this.raio = raio;
+    }    
+
+    public double area(){
+        return Math.PI * raio * raio;
+    }
+}
+
+    
