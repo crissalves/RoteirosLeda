8e705bce86c998222207ea25825d30507d1a1f37
crissalves <cristianalves_cg@hotmail.com>
2023-05-01 10:12:30 -0300

    RO4. Feito e enviado.

--- R04-02-Rot-KLargestQuickSelectFloor-environment/src/main/java/orderStatistic/KLargestOrderStatisticsImpl.java
+++ R04-02-Rot-KLargestQuickSelectFloor-environment/src/main/java/orderStatistic/KLargestOrderStatisticsImpl.java
@@ -1,5 +1,9 @@
 package orderStatistic;
 
+import util.Util;
+
+import java.util.Arrays;
+
 /**
  * Uma implementacao da interface KLargest que usa estatisticas de ordem para 
  * retornar um array com os k maiores elementos de um conjunto de dados/array.
@@ -12,9 +16,7 @@ package orderStatistic;
  * estatisticas de ordem maiores que k. 
  * 
  * Requisitos do algoritmo:
- * - DEVE ser in-place. Porem, voce pode modificar o array original.
- *   Voce pode criar ainda um array de tamanho k que vai armazenar apenas
- *   os elementos a serem retornados.
+ * - DEVE ser in-place. Voce pode modificar o array original
  * - Voce DEVE usar alguma ideia de algoritmo de ordenacao visto em sala 
  *   para calcular estatisticas de ordem.
  * - Se a entrada for invalida, deve-se retornar um array vazio (por exemplo,
@@ -29,24 +31,53 @@ public class KLargestOrderStatisticsImpl<T extends Comparable<T>> implements KLa
 
 	@Override
 	public T[] getKLargest(T[] array, int k) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
-		//este metodo deve obrigatoriamente usar o orderStatistics abaixo.
+
+		T[] eo = (T[]) new Comparable[0]; ;
+
+		if (k >= 1 && array != null && k <= array.length){
+
+			eo = (T[]) new Comparable[k];
+
+			for (int i = 1; i <= k; i++) {
+				eo[i - 1] = orderStatistics(array, i);
+			}
+		}
+
+		return eo;
 	}
 
 	/**
 	 * Metodo que retorna a k-esima estatistica de ordem de um array, usando
-	 * a ideia de algum algoritmo de ordenacao visto em sala. Caso nao exista 
+	 * a ideia de algum algoritmo de ordenacao visto em sala. Caso nao exista
 	 * a k-esima estatistica de ordem, entao retorna null.
-	 * 
+	 *
 	 * Obs: o valor de k deve ser entre 1 e N.
-	 * 
+	 *
 	 * @param array
 	 * @param k
 	 * @return
 	 */
+
 	public T orderStatistics(T[] array, int k){
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");		
+
+		if (k >= 1 && k <= array.length) {
+			for (int i = 0; i < array.length; i++) {
+
+				int smaller = i;
+
+				for (int j = i + 1; j < array.length; j++) {
+					if (array[j].compareTo(array[smaller]) > 0) {
+						smaller = j;
+					}
+				}
+
+				Util.swap(array, i, smaller);
+
+				if (k == i + 1) {
+					return array[i];
+				}
+			}
+		}
+		return null;
 	}
 }
