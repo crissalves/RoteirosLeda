cfb55596f4b56abde65a6c53da364359ff86be23
crissalves <cristianalves_cg@hotmail.com>
2023-05-05 10:36:21 -0300

    RO5 Circular Queue feito

--- R05-02-Rot-TAD-Linear-environment/src/main/java/adt/queue/CircularQueue.java
+++ R05-02-Rot-TAD-Linear-environment/src/main/java/adt/queue/CircularQueue.java
@@ -1,47 +1,72 @@
 package adt.queue;
 
+import javax.lang.model.element.Element;
+
 public class CircularQueue<T> implements Queue<T> {
 
 	private T[] array;
 	private int tail;
 	private int head;
 	private int elements;
+	private int capacity;
 
 	public CircularQueue(int size) {
 		array = (T[]) new Object[size];
 		head = -1;
 		tail = -1;
 		elements = 0;
+		capicity = size;
 	}
 
 	@Override
 	public void enqueue(T element) throws QueueOverflowException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(this.isFull()){
+			throw new RuntimeException("QueueIsFullException");
+		}else{
+			if(head == -1){
+				head = 0;
+			}
+			tail = (tail + 1) % capacity;
+			array[tail] = element;
+			this.elements ++;
+		}
 	}
 
 	@Override
 	public T dequeue() throws QueueUnderflowException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(this.isEmpty()){
+			throw new RuntimeException("QueuIsEmptyException");
+			return -1;
+		}else{
+			element = array[head];
+			if(head == tail){
+				head = -1;
+				tail = -1;
+			}else{
+				head = (head + 1) % capacity;
+			}
+			this.elements--;
+			return element;
+		}
 	}
 
 	@Override
 	public T head() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (this.isEmpty()){
+			throw new RuntimeException("QueuIsEmptyException");
+		}else{
+			return this.array[head];
+		}
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return this.head == -1;
 	}
 
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+		return this.elements == array.length;
+	} 
 
 }
