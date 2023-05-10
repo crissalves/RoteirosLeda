0199ba9fedd5a1f58574b7714309c0e83aa7a5ab
crissalves <cristianalves_cg@hotmail.com>
2023-05-10 18:48:49 -0300

    RO5-02 QueueUsingStack refeito.

--- R05-02-Rot-TAD-Linear-environment/src/main/java/adt/queue/QueueUsingStack.java
+++ R05-02-Rot-TAD-Linear-environment/src/main/java/adt/queue/QueueUsingStack.java
@@ -1,63 +1,60 @@
 package adt.queue;
 
-import java.util.Queue;
-import java.util.Stack;
-
+import adt.stack.Stack;
 import adt.stack.StackImpl;
+import adt.stack.StackOverflowException;
+import adt.stack.StackUnderflowException;
 
 public class QueueUsingStack<T> implements Queue<T> {
-
 	private Stack<T> stack1;
 	private Stack<T> stack2;
 
 	public QueueUsingStack(int size) {
-		this.stack1 = new StackImpl<T>(size);
-		this.stack2 = new StackImpl<T>(size);
+		stack1 = new StackImpl<T>(size);
+		stack2 = new StackImpl<T>(size);
 	}
 
 	@Override
 	public void enqueue(T element) throws QueueOverflowException {
-		if(stack1.isEmpty() && stack2.isEmpty()){
-			throw new RuntimeException("EmptyStackException");
-		}else{
-			while (!stack1.isEmpty()){ 
-            	stack2.push(stack1.pop()); 
-        	} 
+		if (this.isFull()) {
+			throw new QueueOverflowException();
+		}
+		try {
+			if (element != null) {
+				while (!this.stack1.isEmpty()) {
+					this.stack2.push(this.stack1.pop());
+				}
 
-        	stack1.push(element); 
-  
-        	while (!stack2.isEmpty()){ 
-            	stack1.push(stack2.pop()); 
-        	}
-		} 
+				this.stack1.push(element);
+				while (!this.stack2.isEmpty()) {
+					this.stack1.push(this.stack2.pop());
+				}
+			}
+		} catch (StackOverflowException | StackUnderflowException e) {
+			throw new QueueOverflowException();
+		}
+
 	}
 
 	@Override
 	public T dequeue() throws QueueUnderflowException {
-		if (isEmpty()){ 
-        	throw new RuntimeException("EmptyStackException");
-        } else{
-        	if (stack2.isEmpty()) {
-            	while (!stack1.isEmpty()) {
-                	stack2.push(stack1.pop());
-            	}
-        	}
-        	return stack2.pop();
-    	}
+		if (this.isEmpty()) {
+			throw new QueueUnderflowException();
+		}
+		try {
+			return this.stack1.pop();
+		} catch (StackUnderflowException e) {
+			throw new QueueUnderflowException();
+		}
 	}
 
 	@Override
 	public T head() {
-		if (isEmpty()) {
-            throw new IllegalStateException("EmptyStackException.");
-        }else{
-        	if (stack2.isEmpty()) {
-            	while (!stack1.isEmpty()) {
-            		stack2.push(stack1.pop());
-            	}
-        	}
-			return stack2.pop();
+		T head = null;
+		if (!isEmpty()) {
+			head = this.stack1.top();
 		}
+		return head;
 	}
 
 	@Override
