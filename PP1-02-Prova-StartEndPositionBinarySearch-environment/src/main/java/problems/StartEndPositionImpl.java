b1ec5e961583e3f59e82d3489b2d75ce02c18e9d
crissalves <cristianalves_cg@hotmail.com>
2023-05-31 08:31:26 -0300

    PP2.

---
+++ PP1-02-Prova-StartEndPositionBinarySearch-environment/src/main/java/problems/StartEndPositionImpl.java
@@ -0,0 +1,27 @@
+package problems;
+
+/**
+ * Dado um array A ordenado de inteiros de tamanho N e um valor x, possivelmente com mais de uma ocorrencia,
+ * usar busca binária para encontrar as posicoes da primeira e ultima ocorrencias de x em  A.
+
+Return an array of size 2, such that first element = starting position of B in A and second element = ending position of B in A, if B is not found in A return [-1, -1].
+
+ * Restricoes: 
+ * - Seu algoritmo NÃO pode usar memória extra (a nao ser variaveis simples locais e nao de colecao/estrutura) 
+ * - O tempo de seu algoritmo deve ser O(log n).
+ * - Retornar um array com dois elementos [primeiraPosicao,ultimaPosicao], onde primeiraPosicao
+ *   tem o valor do indice da primeira ocorrencia de x em A, e ultimaPosicao tem o valor do indice 
+ *   da ultima ocorrencia de x em A
+ * - Note que se x nao esta em A entao o retorno deve ser [-1,-1]
+ *
+ */
+public class StartEndPositionImpl implements StartEndPosition{
+
+
+	@Override
+	public int[] startEndPosition(Integer[] array, Integer x){
+		//TODO replace this line below with your implementation
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+}
