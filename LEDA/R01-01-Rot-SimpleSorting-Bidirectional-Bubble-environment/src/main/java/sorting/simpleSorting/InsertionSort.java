b5caa30f0750f77fadd8a348ab0d034cba4e20d0
crissalves <cristianalves_cg@hotmail.com>
2024-02-23 12:01:01 -0300

    Revisar

--- LEDA/R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
+++ LEDA/R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -14,11 +14,11 @@ public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {	
 		if( leftIndex < rightIndex){
-			for(int i = rightIndex; i < leftIndex; i++){
+			for(int i = leftIndex; i < rightIndex; i++){
 				int j = i;
-				while(j > 0 && array[j].compareTo(array[j-1]) > 0){
-					Util.swap(array, j, j - 1);
-					j = j - 1;
+				while(j < rightIndex && array[j].compareTo(array[j + 1]) < 0){
+					Util.swap(array, j, j + 1);
+					j = j + 1;
 				}
 			}
 		}
