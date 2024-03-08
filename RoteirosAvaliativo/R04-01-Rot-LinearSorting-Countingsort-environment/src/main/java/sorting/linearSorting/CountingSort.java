a4f7522b4cd3ca2ab100cbf02f02e4032425ccbb
crissalves <cristianalves_cg@hotmail.com>
2024-03-08 11:00:24 -0300

    RO3-CountingSort Terminado.

--- RoteirosAvaliativo/R04-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
+++ RoteirosAvaliativo/R04-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
@@ -1,5 +1,7 @@
 package sorting.linearSorting;
 
+import java.util.Arrays;
+
 import sorting.AbstractSorting;
 
 /**
@@ -17,7 +19,7 @@ public class CountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		if(leftIndex >= rightIndex){
+		if(leftIndex < rightIndex){
 			int maior = 0;
 			for(int i = leftIndex; i <= rightIndex; i++){
 				if(array[i] > maior){
@@ -25,26 +27,27 @@ public class CountingSort extends AbstractSorting<Integer> {
 				}
 			}
 
-			Integer[] arrayContador = new Integer[maior];
-
-			for(int i = leftIndex; i <= rightIndex; i++){
-				arrayContador[array[i] - 1] += 1;
-			}
-
-			for(int i = leftIndex + 1; i <= rightIndex;i++){
-				arrayContador[i] = arrayContador[i-1];
-			}
-
+			Integer[] arrayContador = new Integer[maior + 1];
+			Arrays.fill(arrayContador,0);
 			Integer[] arrayOrdenado = new Integer[array.length];
 
-			for(int i = rightIndex; i >= leftIndex;i++){
-				arrayOrdenado[arrayContador[array[i] - 1] -1]= array[i];
-				arrayContador[array[i] -1] -= 1;
+			for(int i = leftIndex; i <= rightIndex; i++){
+				arrayContador[array[i]]++;
 			}
 
-			for(int i = leftIndex; i<= rightIndex;i++){
-				array[i] = arrayOrdenado[i];
+			for(int i =1; i< arrayContador.length; i++){
+				arrayContador[i] += arrayContador[i-1];
 			}
+
+			for(int i = rightIndex; i >= leftIndex; i--){
+				arrayOrdenado[arrayContador[array[i]] -1]= array[i];
+				arrayContador[array[i]] -= 1;
+			}
+
+			for(int i = leftIndex; i <= rightIndex; i++){
+				array[i]= arrayOrdenado[i];
+			}
+
 		}
 	}
 
