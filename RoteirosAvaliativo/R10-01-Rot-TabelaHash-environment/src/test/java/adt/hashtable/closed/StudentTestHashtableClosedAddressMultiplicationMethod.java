832b1db81b2784b976688d96f1dc581876167957
crissalves <cristianalves_cg@hotmail.com>
2024-04-15 17:16:57 -0300

    RO10- Finalizado.

--- RoteirosAvaliativo/R10-01-Rot-TabelaHash-environment/src/test/java/adt/hashtable/closed/StudentTestHashtableClosedAddressMultiplicationMethod.java
+++ RoteirosAvaliativo/R10-01-Rot-TabelaHash-environment/src/test/java/adt/hashtable/closed/StudentTestHashtableClosedAddressMultiplicationMethod.java
@@ -1,12 +1,12 @@
 package adt.hashtable.closed;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import adt.hashtable.closed.AbstractHashtableClosedAddress;
-import adt.hashtable.closed.HashtableClosedAddressImpl;
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
 
 public class StudentTestHashtableClosedAddressMultiplicationMethod {
